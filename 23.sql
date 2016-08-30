090c7dbba6173355b5801b6e79800331c8aa780b
Tim Maddux <timothymaddux@gmail.com>
2016-08-30 11:22:41 -0500

    Couple moroe.

--- 23.sql
+++ 23.sql
@@ -1,2 +1,11 @@
-/* Provide a query that shows the total sales per country. 
-Which country's customers spent the most. */
+/* Provide a query that shows the total sales per country.*/
+SELECT BillingCountry, COUNT(BillingCountry)
+FROM Invoice
+GROUP BY BillingCountry;
+
+/*Which country's customers spent the most. */
+SELECT BillingCountry, COUNT(BillingCountry), Total
+FROM Invoice
+GROUP BY BillingCountry
+ORDER BY Total DESC
+LIMIT 1
