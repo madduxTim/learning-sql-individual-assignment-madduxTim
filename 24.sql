d8a4d110c29f7b4b115962ebc7a3708027145cce
Tim Maddux <timothymaddux@gmail.com>
2016-08-30 12:37:50 -0500

    All done but 25 & 27.

--- 24.sql
+++ 24.sql
@@ -1,1 +1,11 @@
 /* Shows the most purchased track of 2013. */
+
+SELECT Track.Name, Track.TrackId, COUNT(InvoiceLine.Quantity) "Total"
+FROM Track
+JOIN InvoiceLine
+ON Track.TrackId == InvoiceLine.TrackId
+GROUP BY Track.Name
+ORDER BY Total DESC
+LIMIT 1
+
+/* Not finished. */
